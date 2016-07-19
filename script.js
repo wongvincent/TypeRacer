77a78c76237322de9315582d18513e6a747498a5
Justin <justin.chau623@gmail.com>
2016-07-18 20:32:38 -0700

    - wpm counter - use api key - refactored with less passing around of variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: '//api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=10&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
+    url: '//api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=1000&minLength=3&maxLength=10&limit=10&api_key=ef6dd4f5820f0ae33a0030485610961d4cf34d215edd3293f',
     dataType: 'jsonp',
     jsonpCallback: 'generateWords'
 });
@@ -12,35 +12,52 @@ function generateWords(response) {
 
 var globalTimer;
 
-function startTimer() {
+function Timer() {
     var start = new Date;
+    var seconds;
     globalTimer = setInterval(function() {
-        $('#timer').text(((new Date - start) / parseFloat(1000)).toFixed(3) + " Seconds");
-    }, 1);
+        seconds = ((new Date - start) / parseFloat(1000)).toFixed(2);
+        $('#timer').text(seconds + " Seconds");
+    }, 10);
+    this.getSeconds = function(){
+        return seconds;
+    }
 }
 
-function updateWordsRemaining(i, arrayOfWords) {
-    $('#numberOfWordsRemaining').text(arrayOfWords.length - i);
-    if (i === arrayOfWords.length) {
+function updateWordsRemaining(count) {
+    $('#numberOfWordsRemaining').text(count);
+    if (count === 0) {
         clearInterval(globalTimer);
         var timeStoppedAt = $('#timer').text();
         $('#timer').text("You finished in " + timeStoppedAt);
     }
 }
 
+function WPM() {
+    var timer = new Timer();
+    $('#wpm').text(0);
+    this.wordCounter = 0;
+    this.setWPMText = function() {
+        $('#wpm').text(this.wordCounter / timer.getSeconds() * 60);
+    }
+}
+
 function placeWordsOnScreen(toBeTyped) {
     $(function () {
-        var i = 0;
+        $('#inputtext').focus();
         $('#typethis').text(toBeTyped.join(" "));
-        startTimer();
-        updateWordsRemaining(0, toBeTyped);
+        var wpm = new WPM();
+        updateWordsRemaining(toBeTyped.length);
 
 
         $('#inputtext').on('input', function () {
-            var matchThisWord = i < toBeTyped.length - 1 ? toBeTyped[i] + " " : toBeTyped[i];
+            var matchThisWord = toBeTyped.length > 1 ? toBeTyped[0] + " " : toBeTyped[0];
             if ($('#inputtext').val() === matchThisWord) {
+                wpm.wordCounter += toBeTyped.length > 1 ? ((matchThisWord.length-1)/5) : (matchThisWord.length/5);
+                wpm.setWPMText();
                 $('#inputtext').val('');
-                updateWordsRemaining(++i, toBeTyped);
+                toBeTyped.shift();
+                updateWordsRemaining(toBeTyped.length);
             }
         });
 
